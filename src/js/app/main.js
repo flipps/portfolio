81e700d9cd7658f92727da2a18efb53dade16d3f
flipps <fi.morais.mail@gmail.com>
2016-03-14 23:04:55 -0300

    minor changes

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -1,3 +1,13 @@
 (function() {
- console.log("Hello World");
+
+	$('nav').on('click', function(event){
+		event.preventDefault();
+		var target = event.target.id;
+
+		if(target === 'about') {
+			$('.jobs-container').fadeOut(function(){
+				console.log('Jobs-container faded Out');
+			});
+		}
+	})
 })();
